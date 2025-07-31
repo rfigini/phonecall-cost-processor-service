b739352264b0b484c585bea00736de664a6331ed
Ricardo Figini <ricardo.figini@gmail.com>
2025-07-31 18:50:07 -0300

    Create table if not exists when start service

--- internal/infrastructure/postgres/database.go
+++ internal/infrastructure/postgres/database.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/lib/pq"
 )
@@ -11,5 +12,34 @@ func NewPostgresConnection(dsn string) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	return db, db.Ping()
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	if err := createCallsTableIfNotExists(db); err != nil {
+		return nil, fmt.Errorf("error creating calls table: %w", err)
+	}
+
+	return db, nil
+}
+
+func createCallsTableIfNotExists(db *sql.DB) error {
+	const query = `
+	CREATE TABLE IF NOT EXISTS calls (
+		call_id UUID PRIMARY KEY,
+		caller TEXT,
+		receiver TEXT,
+		duration_in_seconds INT,
+		start_timestamp TIMESTAMPTZ,
+		cost NUMERIC(10, 2),
+		currency TEXT,
+		refunded BOOLEAN DEFAULT false,
+		refund_reason TEXT,
+		status VARCHAR(20) DEFAULT 'PENDING' NOT NULL,
+		processed_at TIMESTAMPTZ DEFAULT now() NOT NULL,
+		CONSTRAINT unique_call_id UNIQUE (call_id)
+	);`
+	_, err := db.Exec(query)
+	return err
 }
